692075866d94716c9fe887d09667da6999b7d171
Alejandro Navarro <a.navarro.montiel@gmail.com>
2026-01-15 13:12:32 +0100

    Introduce a `GLPI_CRONTAB_ENABLED` variable to allow disabling cron (#256)

--- glpi/files/opt/glpi/cron-worker.sh
+++ glpi/files/opt/glpi/cron-worker.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-# Wait for database to be ready before starting cron tasks
-/opt/glpi/entrypoint/wait-for-db.sh cron-worker
+if [ "${GLPI_CRONTAB_ENABLED:-1}" = "1" ]; then
+    echo "[INFO] GLPI cron is enabled. Starting worker tasks..."
+    
+    # Wait for database to be ready before starting cron tasks
+    /opt/glpi/entrypoint/wait-for-db.sh cron-worker
+    
+    # Infinite loop to run GLPI cron tasks every minute
+    while true; do
+        php /var/www/glpi/front/cron.php || echo "[ERROR] GLPI cron execution failed"
+        sleep 60
+    done
 
-# Infinite loop to run GLPI cron tasks every minute
-while true; do
-    php /var/www/glpi/front/cron.php || echo "Cron task failed"
-    sleep 60
-done
+else
+    echo "[INFO] GLPI cron is disabled (GLPI_CRONTAB_ENABLED=0)."
+    # Sleeping forever to keep supervisord happy    
+    tail -f /dev/null
+fi
